b57abfbea2b32a9de741622e986bc53e936bef0e
Thiago Faria <tgroyale@gmail.com>
2025-10-23 23:59:26 -0300

    Parte Thiago pronta

---
+++ src/func.rs
@@ -0,0 +1,74 @@
+use std::io;
+// a palavra-chave `pub` nos permite usar a função no arquivo que está importando este arquivo.
+
+// Função recursiva para calcular o fatorial de um número
+pub fn fatorial(numero: i32) -> i32 {
+    // Essa função será chamada consecutivamente várias vezes até que `numero` seja igual a 0
+    if numero == 0 { return 1 };
+    // Quando acontecer o primeiro retorno (valor 1), ele será multiplicado pelo valor do argumento
+    // da penúltima função chamada, 2 * 1 retorna 2, assim sucessiivamente até chegar ao número
+    // primordial passado para a primeira execução da função `fatorial`
+    numero * fatorial(numero - 1)
+}
+
+// Função para calcular um número elevado a uma potência, sendo `base` o número, e `pot` a potência
+pub fn potencia(base: i32, pot: i32) -> i32 {
+    // Parecida com a função `fatorial`, será chamada várias vezes até que o argumento `pot` seja 0
+    if pot == 0 { return 1 };
+
+    // Quando 1 for retornado, a base irá multiplicar este valor, e depois o resultado será sempre
+    // multiplicado pela base. Assim é como se fosse: 10*1=10, 10*10=100, 100*10=1000
+    base * potencia(base, pot - 1)
+}
+
+/*
+pub fn arranjo() -> i32 {
+
+}
+
+pub fn binomial() -> i32 {
+
+}
+
+pub fn combinacao() -> i32 {
+
+}
+
+pub fn permutacao_simples() -> i32 {
+
+}
+*/
+
+// Calcula a quantidade de permutações com repetição
+pub fn permutacao_repeticao(elementos: i32, repeticoes: Vec<i32>) -> i32 {
+    let mut denominador: i32 = 1;
+
+    // Itera sobre os elementos do vetor `repeticoes` e multiplica o fatorial de cada um pelo valor do denominador
+    for i in repeticoes.iter() {
+        denominador *= fatorial(*i);
+    }
+
+    // Retorna o a quantidade de permutações
+    fatorial(elementos) / denominador
+}
+
+// Pega um valor inteiro a partir da digitação do usuário
+pub fn pega_inteiro() -> i32 {
+    // Cria uma nova string para armazenar a linha do usuário
+    let mut input = String::new();
+
+    // função que pega input do usuário
+    io::stdin()
+        .read_line(&mut input) //precisa passar como referência mutável
+        .expect("Falha na leitura do input do usuário."); // precisa do expect em caso de falha
+
+    // faz um match que verifica se o número, depois de `trim` pode ser convertido para `i32` por
+    // meio de `.parse::<i32>()`
+    match input.trim().parse::<i32>() {
+        // em caso positivo retorna o número digitado
+        Ok(num) => num,
+        // em caso negativo retorna `-1`, neste programa é válido já que não usaremos valores
+        // negativos em momento algum.
+        Err(_) => -1,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+// cria um módulo para o arquivo `func.rs`
+mod func;
+// importa tudo (*) do módulo `func`
+use func::*;
+
+
+fn main() {
+    loop {
+        println!("\nEscolha a operação que deseja fazer:");
+        println!("1. Fatorial\n2. Arranjo\n3. Binomial\n4. Combinação\n5. Permutação simples\n6. Permutação com repeticões\n0. Sair");
+        let input: i32 = pega_inteiro();
+
+        if input == -1 {
+            println!("Opção inválida. Tente novamente.");
+            continue;
+        }
+
+        match input {
+            0 => {
+                break;
+            },
+            1 => {
+                println!("\n=======FATORIAL=======");
+                println!("Digite um número para ver seu fatorial:");
+                let numero: i32 = pega_inteiro();
+
+                // Verifica se o número digitado é negativo
+                if numero < 0 {
+                    println!("Número inválido. Tente novamente.");
+                    continue;
+                }
+
+                let fat: i32 = fatorial(numero);
+                
+                println!("O fatorial de {} é = {}", numero, fat);
+            }
+            2 => {
+                println!("\n=======ARRANJO=======");
+                println!("Digite a quantidade de elementos que deseja arranjar:");
+                let elementos: i32 = pega_inteiro();
+                println!("Digite a quantidade de lugares em que os números serão arranjados:");
+                let lugares: i32 = pega_inteiro();
+            
+                // Verifica se algum número digitado foi menor que 1
+                if elementos < 1 || lugares < 1 {
+                    println!("Valores menores que 1 não são válidos para elementos e lugares no arranjo. Tente novamente.");
+                    continue;
+                }
+
+                //let arrj: i32 = arranjo();
+                //println!("O arranjo de {} elementos em {} lugares é = {}", elementos, lugares, arrj);
+            },
+            3 => {
+                println!("\n=======BINOMIAL=======");
+                println!("Digite a ordem do número binomial:");
+                let n: i32 = pega_inteiro();
+                println!("Digite a classe do número binomial:");
+                let k: i32 = pega_inteiro();
+
+                if n < 1 || k < 1 {
+                    println!("Valores menores que 1 não são válidos para ordem ou classe de um número binomial. Tente novamente.");
+                    continue;
+                }
+
+                //let binomial: i32 = binomial();
+                //println!("O número binomial de ordem {} e classe {} é = {}", n, k, binomial);
+            },
+            4 => {
+                println!("\n=======COMBINAÇÃO=======");
+                println!("Digite o número de elementos a serem combinados:");
+                let elementos: i32 = pega_inteiro();
+                println!("Digite o número de lugares em que os elementos serão combinados:");
+                let lugares: i32 = pega_inteiro();
+
+                if elementos < 1 || lugares < 1 {
+                    println!("Valores menores que 1 não são válidos para o número de elementos ou lugares. Tente novamente.");
+                    continue;
+                }
+
+                //let combinacao: i32 = combinacao();
+                //println!("O número de combinações com {} elementos tomados de {} em {} é = {}", elementos, lugares, lugares, combinacao);
+            },
+            5 => {
+                println!("\n=======PERMUTAÇÃO SIMPLES=======");
+                println!("Digite a quantidade de elementos que serão permutados:");
+                let elementos: i32 = pega_inteiro();
+
+                if elementos < 1 {
+                    println!("Valor menor 1 não é válido para o número de elementos na permutação. Tente novamente.");
+                    continue;
+                }
+
+                //let permutacao = permutacao();
+                //println!("O número de permutações possíveis com {} elementos é = {}", elementos, permutacao);
+            },
+            6 => {
+                println!("\n=======PERMUTAÇÃO COM REPETIÇÃO=======");
+                println!("Digite a quantidade de elementos que serão permutados:");
+                let elementos: i32 = pega_inteiro();
+                println!("Digite a quantidade de elementos que se repetem:");
+                let quant: i32 = pega_inteiro();
+                let mut repeticoes: Vec<i32> = Vec::new();  // cria um vetor para armazenar valores i32
+
+                if elementos < 1 || quant < 1{
+                    println!("Valores menores que 1 não são válidos para elementos, quantidade de repetições e repetições. Tente novamente.");
+                    continue;
+                }
+
+                // Iteração para pegar números e enfiá-los no vetor.
+                for i in 1..=quant {
+                    println!("Digite a quantidade de repetições do elemento {}:", i);
+                    repeticoes.push(pega_inteiro());
+                }
+                
+                let permutacao: i32 = permutacao_repeticao(elementos, repeticoes);
+
+                println!("A quantidade de permutações de {} elementos {} elementos repetidos é = {}", elementos, quant, permutacao);
+            },
+            _ => {  // mesmo que default em C, mas obrigatório neste caso.
+                println!("Opção inválida. Tente novamente.");
+                continue;
+            }
+        }
+    }
+
+    println!("Até mais!");
+}
